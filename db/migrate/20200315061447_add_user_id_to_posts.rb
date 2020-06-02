98556ae3bb10c328b19fd89b49241949b0e69020
yasunari <heaty2525@yahoo.co.jp>
2020-06-02 21:29:28 +0900

    post-migration change

--- db/migrate/20200315061447_add_user_id_to_posts.rb
+++ db/migrate/20200315061447_add_user_id_to_posts.rb
@@ -1,10 +1,5 @@
 class AddUserIdToPosts < ActiveRecord::Migration[5.2]
-  def up
-    execute 'DELETE FROM posts;'
+  def change
     add_reference :posts, :user, null: false, index: true
-  end
-
-  def down
-    remove_reference :posts, :user,index: true
   end
 end
